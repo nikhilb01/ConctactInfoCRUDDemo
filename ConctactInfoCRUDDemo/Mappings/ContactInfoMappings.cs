f7e35619a8c9110a1303965decfb5159bff4c0af
SILICUS\NBansode <Nikhilbansode12@gmail.com>
2019-09-25 13:21:17 +0530

    Add project files.

---
+++ ConctactInfoCRUDDemo/Mappings/ContactInfoMappings.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using ConctactInfoCRUDDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ConctactInfoCRUDDemo.Mappings
+{
+    public class ContactInfoMappings : Profile
+    {
+        public ContactInfoMappings()
+        {
+            CreateMap<ContactInfoViewModel, ContactInfo.DBEntities.Entities.ContactInfo>().ReverseMap();
+        }
+    }
+}
